9128fe6f6db3aa8486d77ced1ac7fb5d35696203
camcimber <camryn.cimber@ufl.edu>
2023-04-18 21:24:17 -0400

    progress parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+class Minerals{
+private:
+    double carb, protein, sodium, fiber, sugar;
+    string category, description;
+
+public:
+   Minerals(){
+       carb = 0.0;
+       protein = 0.0;
+       sodium = 0.0;
+       fiber = 0.0;
+       sugar = 0.0;
+   }
+   void parsing(){
+        ifstream inputFile;
+        inputFile.open("nutritional-facts.csv");
+
+        string line = "";
+        while(getline(inputFile, line)){
+
+            stringstream inputString(line);
+            getline(inputString, category, ',');
+
+        }
+   }
+
+
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Minerals nutrition;
     return 0;
 }
