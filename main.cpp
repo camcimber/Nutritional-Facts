49b2d394949d2af833307984f004656a9923f085
camcimber <camryn.cimber@ufl.edu>
2023-04-21 22:41:02 -0400

    parsing progress

--- main.cpp
+++ main.cpp
@@ -7,18 +7,12 @@ using namespace std;
 
 class Minerals{
 private:
-    double carb, protein, sodium, fiber, sugar;
+    //string carb, protein, sodium, fiber, sugar;
     string category, description;
     unsigned int finder, finder2; // used for parsing "description"
 
 public:
-   Minerals(){
-       carb = 0.0;
-       protein = 0.0;
-       sodium = 0.0;
-       fiber = 0.0;
-       sugar = 0.0;
-   }
+
    void parsing(){
         ifstream inputFile;
         inputFile.open("nutritional-facts.csv");
@@ -30,28 +24,28 @@ public:
             getline(inputString, category, ',');
 
             //parse to get description
-            finder = line.find('"');
+            getline(inputString, description, ',');
+
             //some descriptions have quotes and other do not
-            if(finder){
-                description = line.substr(finder+1);
+            if (description.front() == '"') {
+                finder = line.find('"');
+                description = line.substr( finder + 1);
                 finder2 = description.find('"');
                 description = description.substr(0, finder2);
             }
-            else{
-                getline(inputString, description, ',');
+
+            //parse to get carbohydrates
+            string carb = "";
+            for (int i = 0; i < 6; i++) {
+                getline(inputString, carb, ',');
             }
-          //  cout << "category: " << category << " description : " << description << endl;
 
-            //parse to get carbohydrates -> work in progress
-//            string temp;
-//            for (int i = 0; i < 6; i++) {
-//                getline(inputString, temp, ',');
-//            }
-
-            // Extract the number after the 6th comma
-            //getline(inputString, temp, ',');
-            //int number = stoi(temp);
-
+            //parse for cholesterol -> in progress, not printing correct value
+            string cholesterol = "";
+            for (int i = 0; i < 7; i++) {
+                getline(inputString, cholesterol, ',');
+            }
+            cout << cholesterol << endl;
 
         }
    }
