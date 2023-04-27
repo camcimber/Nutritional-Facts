1ff8b37d05105bbe30de4c72de21734fab391432
MSSFree88 <slovakm@ufl.edu>
2023-04-27 02:54:26 -0400

    Added Functions.h
    Moved all non-sorting/ Food class functions other than main to Functions.h. Cleaned up includes on main. Changed map to an unordered_map.

--- main.cpp
+++ main.cpp
@@ -1,158 +1,21 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <queue>
-#include <thread>
+#include <unordered_map>
 #include <chrono>
-#include <iomanip>
 #include "Food.h"
 #include "heapSort.h"
 #include "timSort.h"
+#include "Functions.h"
 
 using namespace std;
-
-void parseData(map<string, vector<Food>>& data, string fileName) {
-    ifstream file(fileName);
-
-    // Read and discard the header row
-    string line;
-    getline(file, line);
-
-    while (getline(file, line)) {
-        stringstream ss(line);
-        vector<string> row;
-
-        // Read the relevant columns
-        string item;
-
-        // Category
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Description
-        getline(ss, item, ',');
-        // If the description is in quotes, read until the end of the quotes
-        if (item.front() == '\"') {
-            // Read until the end of the quotes
-            while (item.back() != '\"') {
-                string temp;
-                getline(ss, temp, ',');
-                item += "," + temp;
-            }
-
-            // Remove the quotes from the description
-            item.erase(0, 1);
-            item.pop_back();
-            row.push_back(item);
-
-        } else {
-            row.push_back(item);
-        }
-
-        // Skip the next 4 columns
-        for (int i = 0; i < 4; i++) {
-            getline(ss, item, ',');
-        }
-
-        // Carbohydrates
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Skip the next 2 columns
-        for (int i = 0; i < 2; i++) {
-            getline(ss, item, ',');
-        }
-
-        // Fiber
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Skip the next 3 columns
-        for (int i = 0; i < 3; i++) {
-            getline(ss, item, ',');
-        }
-
-        // Protein
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Skip the next 3 columns
-        for (int i = 0; i < 3; i++) {
-            getline(ss, item, ',');
-        }
-
-        // Sugar
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Skip the next 12 columns
-        for (int i = 0; i < 12; i++) {
-            getline(ss, item, ',');
-        }
-
-        // Sodium
-        getline(ss, item, ',');
-        row.push_back(item);
-
-        // Extract the data from the row
-        string category = row[0];
-        string description = row[1];
-
-        // Convert the string data to double
-        double carbohydrates = stod(row[2]);
-        double fiber = stod(row[3]);
-        double protein = stod(row[4]);
-        double sugar = stod(row[5]);
-        double sodium = stod(row[6]);
-
-        // Create a new Food object and add it to the vector
-        Food food(category, description, carbohydrates, fiber, protein, sugar, sodium);
-
-        // Add the food to the map
-        data[category].push_back(food);
-
-    }
-}
-
-void printSortedData(const vector<Food>& data, int fieldNumber, string measurement, string macroChosen) {
-    
-    // Capitalize the first letter of the macro
-    macroChosen[0] = toupper(macroChosen[0]);
-
-    //get the longest description length for formatting
-    size_t maxDesLength = 0;
-    for (const auto& food : data) {
-        size_t descriptionLength = food.description().size();
-        if (descriptionLength > maxDesLength) {
-            maxDesLength = descriptionLength;
-        }
-    }
-    // Print the data of the chosen category
-    cout << fixed << setprecision(2);
-    cout << left;  // Set left alignment for the description field
-
-    size_t fieldWidth = maxDesLength + 4;
-
-    // FIXME: Print the data in a table format
-    cout << "Rank" << setw(fieldWidth) << "Description" << setw(8) << right << macroChosen << " " << setw(4) << left << "(" << measurement << ")" << endl;
-    for (int i = 0; i < data.size(); i++) {
-        cout << setw(3) << i + 1 << ". ";
-        cout << setw(fieldWidth) << data[i].description()
-        << setw(8) << right << data[i].fieldValueGetter(fieldNumber) << " " << setw(4) << left << measurement << endl;
-    }
-}
 
 int main() {
     // Welcome message
     cout << "Welcome to Macro-Tracker! " << endl;
 
     // Create a map to store the data
-    map<string, vector<Food>> data;
+    unordered_map<string, vector<Food>> data;
 
     // Parse the data
     parseData(data, "food.csv");
